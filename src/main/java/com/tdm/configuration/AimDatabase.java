438155447ceae75b6e96b68df20258a8e3c88fe3
sanodamo <sanodamo@gmail.com>
2019-01-07 23:34:58 -0600

    Update AimDatabase.java

--- src/main/java/com/tdm/configuration/AimDatabase.java
+++ src/main/java/com/tdm/configuration/AimDatabase.java
@@ -1,9 +1,9 @@
-package com.preauth.configuration;
+package com.tdm.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix="spring.datasource.preauth.aim-member")
-public class AimDatabase extends DatabaseProperties  {	
+@ConfigurationProperties(prefix="spring.datasource.preauth.member")
+public class Database extends DatabaseProperties  {	
 }
